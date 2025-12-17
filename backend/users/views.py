472df72789be175cd150f49ebfc9c495b5cf217b
Sarah El-Shinnawy <s.Ahmed2397@nu.edu.eg>
2025-12-17 11:28:33 +0200

    added the core App in settings.py in theroot folder and organizing backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt 
 import json
-from .models import Student, Instructor
+from .models import User
 
 @csrf_exempt
 def signup(request):
