91890002839cbfd921918f1b5e4ba02afb50570a
Sarah El-Shinnawy <s.Ahmed2397@nu.edu.eg>
2025-12-17 12:33:38 +0200

    finalized user models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -21,7 +21,7 @@ class User(AbstractUser):
     # Optional profile fields
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    website = models.URLField(blank=True)
+    
     
     def __str__(self):
         return self.username
