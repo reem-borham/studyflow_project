053715b4932b166dd645126153feff2eba16eea3
Sarah El-Shinnawy <s.Ahmed2397@nu.edu.eg>
2025-12-17 09:54:10 +0200

    Added core App for reausable components like Tags, Votes, Comments, Notifications, Reports/Flags

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,2 +1,3 @@
 from django.contrib import admin
-from .models import User
+
+# Register your models here.
--- backend/studyflow/settings.py
+++ backend/studyflow/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'core',
 ]
 
 AUTH_USER_MODEL = 'users.User'
