fb06aa43b4269e6d96614aa06b8ce61b6da736c2
Sarah El-Shinnawy <s.Ahmed2397@nu.edu.eg>
2025-12-19 18:22:56 +0200

    added dashboard/ and tested the API for the following functionalities : questions (update / delete) answers dashboard login register

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserDashboardView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('dashboard/', UserDashboardView.as_view(), name='user_dashboard'),
 ]
